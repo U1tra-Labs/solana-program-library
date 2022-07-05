5ecf4f6a0b6308334cb7ce61a337e7914507c591
wally <63522929+stephenwilliams1990@users.noreply.github.com>
2022-07-05 18:27:21 -0500

    Vercel

--- token-lending/app/src/components/Positions.tsx
+++ token-lending/app/src/components/Positions.tsx
@@ -83,7 +83,6 @@ export default function Positions({
                             callback={callback}
                             provider={provider}
                         />
-                        
                     </ButtonGroup>
                 </td>
             </tr>
